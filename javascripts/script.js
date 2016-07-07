3176146fdd371d8b2fbdfdad31403e7277f3a34f
Nicole Zhu <nzhu94@gmail.com>
2016-07-07 17:15:17 -0500

    add aabout, media, homepage copy

---
+++ javascripts/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	resizeDiv();
+});
+
+$(window).resize(function(){
+	resizeDiv();
+});
+
+function resizeDiv() {
+	if ($(window).width() <= 768) {
+		var windowHeight = $(window).height() - $('footer').height() - $('nav').height();
+	  	$('#animation').css('min-height', windowHeight );
+	} else {
+		$('#animation').css('min-height', '100%' );
+	}
+}
+;
